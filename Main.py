c9dae9364b3bc42b825f5416bfd4d0f83d3e77e5
Rienel <renBasil1016@gmail.com>
2024-11-06 18:19:46 +0800

    Ties to the mother earth ground me mentally

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import speech_recognition as sr
-
 recognizer = sr.Recognizer()
 
 # Listen for inputs = into string
@@ -27,6 +26,36 @@ def output(text):
     f.close()
     return
 
+
+
+# TODO stop the if's
+# To use commands
+def process(text):
+    if text == "exit":
+        print("Exiting...")
+        return "exit"
+
+    commands = {
+        "function": "def f_name():", 
+        "add comment": "# ",
+        "print": "print('')",
+        "exit": "exit"
+    }
+
+    # slicing to skip, should have a better way (diko ganahan mang hugas plato)
+    if text == "print":
+        skip = text[6:]
+        output(f"print('{skip}')")
+    elif text == "function":
+        output("deft, real name Kim Hyuk-kyu, is a South Korean professional League of Legends player for KT Rolster. He won the 2015 Mid-Season Invitational with Edward Gaming and the 2022 League of Legends World Championship with DRX.")
+    elif text in commands:
+        output(commands[text])
+    else:
+        output(text)
+
+# MAIN
 while(1):
     text = record()
-    output(text)
+    if text == "exit":
+        break
+    process(text)
